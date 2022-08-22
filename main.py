54de98d26e06829b210e1e994235f692f4178ae1
firas122 <firas122@outlook.fr>
2022-08-22 11:33:21 +0100

    api push

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import re
-inp = input('write your string to clean  : ')
-klem = ["3asba","nayek","zebi"]
+import flask
+from flask import request
 
-print(inp)
-r = inp.split(" ")
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+@app.route('/do', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        p = request.form.get('string')
+    r = p.split(" ")
 
 
-for i in range(len(r)):
-    searchObj1 = re.match(r'[z,Z]{1}[e,E]+[b,B]+[i,I]+', r[i])
-    searchObj2 = re.match(r'[m,M]*[n,N]{1}[a,A]+[y,Y]+[e,E]+[k,K]+', r[i])
-    searchObj3 = re.match(r'[3]{1}[a,A]+[s,S]+[b,B]+[a,A]+', r[i])
-    searchObj4 = re.match(r'[3]{1}[a,A]+[s,S]+[b,B]+[a,A]+', r[i])
-    searchObj5 = re.match(r'[9]{1}[a,A]+[7]+[b,B]+[a,A]+', r[i])
+    for i in range(len(r)):
+        searchObj1 = re.match(r'[z,Z]{1}[e,E]+[b,B]+[i,I]+', r[i])
+        searchObj2 = re.match(r'[m,M]*[n,N]{1}[a,A]+[y,Y]+[e,E]+[k,K]+', r[i])
+        searchObj3 = re.match(r'[3]{1}[a,A]+[s,S]+[b,B]+[a,A]+', r[i])
+        searchObj4 = re.match(r'[3]{1}[a,A]+[s,S]+[b,B]+[a,A]+', r[i])
+        searchObj5 = re.match(r'[9]{1}[a,A]+[7]+[b,B]+[a,A]+', r[i])
 
-    if searchObj1 or searchObj2 or  searchObj3 or searchObj4 or searchObj5:
-        s = "*"*len(r[i])
-        r[i]=s
+        if searchObj1 or searchObj2 or  searchObj3 or searchObj4 or searchObj5:
+            s = "*"*len(r[i])
+            r[i] = s
 
-str = ' '
+    str = ' '
 
-print(str.join(r))
+    return(str.join(r))
